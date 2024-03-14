d59ea8ad2aaa1f5cd2f2099fdba25eecf86ca9c7
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-03-15 01:22:04 +0200

    update

--- src/assets/dummyData.js
+++ src/assets/dummyData.js
@@ -1,3 +1,6 @@
+import image1 from "./images/image1.jpg";
+import image2 from "./images/image2.jpg";
+
 export const userData = [
   {
     name: "Jan",
@@ -67,91 +70,81 @@ export const productData = [
 export const userRows = [
   {
     id: 1,
-    username: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    email: "jon@gmail.com",
+    username: "Ziad Mohamed",
+    avatar: image1,
+    email: "ziad@gmail.com",
     status: "active",
     transaction: "$120.00",
   },
   {
     id: 2,
-    username: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    email: "jon@gmail.com",
+    username: "Ziad Mohamed",
+    avatar: image1,
+    email: "ziad@gmail.com",
     status: "active",
     transaction: "$120.00",
   },
   {
     id: 3,
-    username: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    email: "jon@gmail.com",
+    username: "Ziad Mohamed",
+    avatar: image1,
+    email: "ziad@gmail.com",
     status: "active",
     transaction: "$120.00",
   },
   {
     id: 4,
-    username: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    email: "jon@gmail.com",
+    username: "Ziad Mohamed",
+    avatar: image1,
+    email: "ziad@gmail.com",
     status: "active",
     transaction: "$120.00",
   },
   {
     id: 5,
-    username: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    email: "jon@gmail.com",
+    username: "Ziad Mohamed",
+    avatar: image1,
+    email: "ziad@gmail.com",
     status: "active",
     transaction: "$120.00",
   },
   {
     id: 6,
-    username: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    email: "jon@gmail.com",
+    username: "Ziad Mohamed",
+    avatar: image1,
+    email: "ziad@gmail.com",
     status: "active",
     transaction: "$120.00",
   },
   {
     id: 7,
-    username: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    email: "jon@gmail.com",
+    username: "Ziad Mohamed",
+    avatar: image1,
+    email: "ziad@gmail.com",
     status: "active",
     transaction: "$120.00",
   },
   {
     id: 8,
-    username: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    email: "jon@gmail.com",
+    username: "Ziad Mohamed",
+    avatar: image1,
+    email: "ziad@gmail.com",
     status: "active",
     transaction: "$120.00",
   },
   {
     id: 9,
-    username: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    email: "jon@gmail.com",
+    username: "Ziad Mohamed",
+    avatar: image1,
+    email: "ziad@gmail.com",
     status: "active",
     transaction: "$120.00",
   },
   {
     id: 10,
-    username: "Jon Snow",
-    avatar:
-      "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    email: "jon@gmail.com",
+    username: "Ziad Mohamed",
+    avatar: image1,
+    email: "ziad@gmail.com",
     status: "active",
     transaction: "$120.00",
   },
@@ -161,7 +154,7 @@ export const productRows = [
   {
     id: 1,
     name: "Apple Airpods",
-    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    img: image2,
     stock: 123,
     status: "active",
     price: "$120.00",
@@ -169,7 +162,7 @@ export const productRows = [
   {
     id: 2,
     name: "Apple Airpods",
-    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    img: image2,
     stock: 123,
     status: "active",
     price: "$120.00",
@@ -177,7 +170,7 @@ export const productRows = [
   {
     id: 3,
     name: "Apple Airpods",
-    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    img: image2,
     stock: 123,
     status: "active",
     price: "$120.00",
@@ -185,7 +178,7 @@ export const productRows = [
   {
     id: 4,
     name: "Apple Airpods",
-    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    img: image2,
     stock: 123,
     status: "active",
     price: "$120.00",
@@ -193,7 +186,7 @@ export const productRows = [
   {
     id: 5,
     name: "Apple Airpods",
-    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    img: image2,
     stock: 123,
     status: "active",
     price: "$120.00",
@@ -201,7 +194,7 @@ export const productRows = [
   {
     id: 6,
     name: "Apple Airpods",
-    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    img: image2,
     stock: 123,
     status: "active",
     price: "$120.00",
@@ -209,7 +202,7 @@ export const productRows = [
   {
     id: 7,
     name: "Apple Airpods",
-    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    img: image2,
     stock: 123,
     status: "active",
     price: "$120.00",
@@ -217,7 +210,7 @@ export const productRows = [
   {
     id: 8,
     name: "Apple Airpods",
-    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    img: image2,
     stock: 123,
     status: "active",
     price: "$120.00",
@@ -225,7 +218,7 @@ export const productRows = [
   {
     id: 9,
     name: "Apple Airpods",
-    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    img: image2,
     stock: 123,
     status: "active",
     price: "$120.00",
@@ -233,7 +226,7 @@ export const productRows = [
   {
     id: 10,
     name: "Apple Airpods",
-    img: "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    img: image2,
     stock: 123,
     status: "active",
     price: "$120.00",
