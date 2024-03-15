14a8397250072a16d1589e5fceec06853f9c75c1
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-03-15 03:28:08 +0200

    update

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,7 +1,35 @@
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+
+import navbarImage from "../../assets/images/navbar.jpg";
+
 import "./topbar.css";
 
 const Topbar = () => {
-  return <div>Topbar</div>;
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Ziad ERP</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNoneOutlinedIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <LanguageOutlinedIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <SettingsOutlinedIcon />
+          </div>
+          <img src={navbarImage} alt="" className="topAvatar" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Topbar;
