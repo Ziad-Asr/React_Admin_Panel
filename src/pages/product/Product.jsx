634dece791a9975ad2bce395ed2f2cbf65c53205
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-03-16 20:36:27 +0200

    update

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,7 +1,79 @@
+import { Link } from "react-router-dom";
+import { productData } from "../../assets/dummyData";
+import Chart from "../../components/chart/Chart";
+import PublishIcon from "@mui/icons-material/Publish";
+import productImage from "../../assets/images/image2.jpg";
+
 import "./product.css";
 
 const Product = () => {
-  return <div className="product">Product</div>;
+  return (
+    <div className="product">
+      <div className="productTitleContainer">
+        <h1 className="productTitle">Product</h1>
+        <Link to="/newproduct">
+          <button className="productAddButton">Create</button>
+        </Link>
+      </div>
+      <div className="productTop">
+        <div className="productTopLeft">
+          <Chart data={productData} dataKey="Sales" title="Sales Performance" />
+        </div>
+        <div className="productTopRight">
+          <div className="productInfoTop">
+            <img src={productImage} alt="" className="productInfoImg" />
+            <span className="productName">Apple Airpods</span>
+          </div>
+          <div className="productInfoBottom">
+            <div className="productInfoItem">
+              <span className="productInfoKey">id:</span>
+              <span className="productInfoValue">123</span>
+            </div>
+            <div className="productInfoItem">
+              <span className="productInfoKey">sales:</span>
+              <span className="productInfoValue">5123</span>
+            </div>
+            <div className="productInfoItem">
+              <span className="productInfoKey">active:</span>
+              <span className="productInfoValue">yes</span>
+            </div>
+            <div className="productInfoItem">
+              <span className="productInfoKey">in stock:</span>
+              <span className="productInfoValue">no</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="productBottom">
+        <form className="productForm">
+          <div className="productFormLeft">
+            <label>Product Name</label>
+            <input type="text" placeholder="Apple AirPod" />
+            <label>In Stock</label>
+            <select name="inStock" id="idStock">
+              <option value="yes">Yes</option>
+              <option value="no">No</option>
+            </select>
+            <label>Active</label>
+            <select name="active" id="active">
+              <option value="yes">Yes</option>
+              <option value="no">No</option>
+            </select>
+          </div>
+          <div className="productFormRight">
+            <div className="productUpload">
+              <img src={productImage} alt="" className="productUploadImg" />
+              <label for="file">
+                <PublishIcon />
+              </label>
+              <input type="file" id="file" style={{ display: "none" }} />
+            </div>
+            <button className="productButton">Update</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
