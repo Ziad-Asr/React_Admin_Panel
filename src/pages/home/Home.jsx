dd2250ce52fd2f24b54e7b1ce0edfcf4c8314afa
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-03-16 17:15:29 +0200

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,20 @@
+import Chart from "../../components/chart/Chart";
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import { userData } from "../../assets/dummyData";
 import "./home.css";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
+    </div>
+  );
 };
 
 export default Home;
